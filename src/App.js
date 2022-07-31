1034b96f51a37f875daf837668b216773d926779
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-08-01 09:57:24 +1000

    package installation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <i class="fa-solid fa-people-carry-box"></i>
+      <Button variant="danger">Hello</Button>
     </div>
   );
 }
