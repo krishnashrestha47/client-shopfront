784b5444764b058def394cbad8be79a128cfdeb2
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-08-24 13:26:43 +1000

    store and slice for products

--- src/components/products/productSlice.js
+++ src/components/products/productSlice.js
@@ -1,7 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  products: [],
+};
 const productSlice = createSlice({
   name: "product",
   initialState,
-  reducers: {},
+  reducers: {
+    getAllProducts: (state, { payload }) => {
+      state.products = payload;
+    },
+  },
 });
+
+const { reducer, actions } = productSlice;
+
+export const { getAllProducts } = actions;
+
+export default reducer;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../src/components/cart/cartSlice";
+import productReducer from "../src/components/products/productSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    products: productReducer,
   },
 });
 
